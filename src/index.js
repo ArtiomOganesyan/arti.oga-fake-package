f527b151c10ae735c6522f521ffd77f6766a44c3
ArtiomOganesyan <artiomoganesyan@gmail.com>
2022-11-13 14:49:18 +0500

    readme

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 const generateUser = require('./generateUser');
 
-console.log(generateUser({ amount: 12, specific: { age: 35, firstName: 'Bob' }, attributes: ['age', 'email'] }));
+console.log(
+  generateUser({
+    amount: 4,
+    specific: { age: 53, firstName: 'Nancy' },
+    attributes: ['age', 'email'],
+  }),
+);
 
 module.exports = {
   generateUser,
