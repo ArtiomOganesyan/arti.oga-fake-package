df8d921d3f8d8cde46a1e81dba482caed5557f0b
ArtiomOganesyan <artiomoganesyan@gmail.com>
2022-11-13 14:50:19 +0500

    remove log

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 const generateUser = require('./generateUser');
 
-console.log(
-  generateUser({
-    amount: 4,
-    specific: { age: 53, firstName: 'Nancy' },
-    attributes: ['age', 'email'],
-  }),
-);
-
 module.exports = {
   generateUser,
 };
